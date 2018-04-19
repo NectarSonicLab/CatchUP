b63564a49a335c4bc60c000f728216731a834450
NectarSonicLab <thomaspiaczinski@gmail.com>
2018-04-19 16:17:03 +0200

    update

--- app/src/main/java/fr/nectarlab/catchup/EventSetup.java
+++ app/src/main/java/fr/nectarlab/catchup/EventSetup.java
@@ -1,11 +1,84 @@
 package fr.nectarlab.catchup;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 /**
  * Created by ThomasBene on 4/19/2018.
  */
 
 public class EventSetup extends AppCompatActivity{
+    private final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0;
+    int PLACE_PICKER_REQUEST = 1;
+    TextView retrievePlace;
+    @Override
+    public void onCreate (Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.event_setup);
+        retrievePlace = findViewById(R.id.eventSetup_retrievePlace_tv);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+    }
+    public void pickPlace(View v){
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+        try{
+            startActivityForResult(builder.build(EventSetup.this), PLACE_PICKER_REQUEST);
+        }
+        catch (GooglePlayServicesRepairableException e){
+            e.printStackTrace();
+        }
+        catch (GooglePlayServicesNotAvailableException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onActivityResult (int requestCode, int resultCode, Intent data){
+        if(requestCode == PLACE_PICKER_REQUEST){
+            if(resultCode == RESULT_OK){
+                Place place = PlacePicker.getPlace(this, data);
+                retrievePlace.setText(place.getAddress());
+            }
+        }
+    }
 
 }
